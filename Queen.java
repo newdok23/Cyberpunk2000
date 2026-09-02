54cb1194625e73a6bb9822b637dfe21db9d3d2e9
CZ800 <chris.zhou@students.mq.edu.au>
2026-09-02 15:07:31 +1000

    Minor updates to Queen.java

--- Queen.java
+++ Queen.java
@@ -1,9 +1,37 @@
+import java.io.Console;
+
 class Queen {
     
     int eat ;
     
     void setEat(int eat) { this.eat = eat; }
     int getEat() { return eat ; }
+
+    public Worker prodWorker() {
+        if (energy < WORKER_COST) {
+            System.out.println("Queen: not enough energy to spawn a worker.");
+            return null;
+        }
+
+        energy -= WORKER_COST;
+        Worker worker = new Worker(nextAntId++);
+        workers.add(worker);
+        System.out.println("Queen spawned " + worker + ". Energy left: " + energy);
+        return worker;
+    }
+
+    public Scout prodScout() {
+        if (energy < SCOUT_COST) {
+            System.out.println("Queen: not enough energy to spawn a scout.");
+            return null;
+        }
+
+        energy -= SCOUT_COST;
+        Scout scout = new Scout(nextAntId++);
+        scouts.add(scout);
+        System.out.println("Queen spawned " + scout + ". Energy left: " + energy);
+        return scout;
+    }
 
 }
 
